82e3c502f2c26cea19a70f7a156d2b7b0929b109
sunu-nandakumar <appu1998.nandakumar@gmail.com>
2021-05-28 20:16:58 +0530

    ADDING the second part of the code.

--- Binary.java
+++ Binary.java
@@ -7,20 +7,29 @@ public class Binary {
 		System.out.println("Enter number you want to convert to binary");
 		int n = myObj.nextInt();
 		
-		toBinary(n);
-		System.out.println(); 
-		
+		int number = toBinary(n);
+		System.out.println(+ number ); 
+		int x = swapNibbles(n);
+		System.out.println( + x);
 		
 
 	}
-	static void toBinary(int n)
+	public static int toBinary(int n)
 	  {
 		
         if (n > 1) {
             toBinary(n / 2);
         }
-       
-        System.out.print(n % 2);
+        int number = n % 2;
+        
+        System.out.print(number);
+        return number ;
 	  }
-	 
+	static int swapNibbles(int x)
+	{
+	    
+	  int value = ((x & 0x0F) << 4 | (x & 0xF0) >> 4);
+	  
+	   return value;
+	}
 }
